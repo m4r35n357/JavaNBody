0088ab1ceef831405492546f90742f63d72b228d
Ian Smith <m4r35n357@gmail.com>
2013-05-16 10:51:45 +0100

    testing momentum update like javascript version

--- src/main/java/uk/me/doitto/orbits/PhaseSpace.java
+++ src/main/java/uk/me/doitto/orbits/PhaseSpace.java
@@ -18,6 +18,36 @@ public enum PhaseSpace {
 			}
 		}
 	},	
+	P1 {  // Momentum
+		@Override
+		void update (Symplectic s, double c) {
+			for (int i = 0; i < s.np; i++) {
+				Particle a = s.particles.get(i);
+				double am = a.mass;
+				double aQx = a.qX;
+				double aQy = a.qY;
+				double aQz = a.qZ;
+				for (int j = 0; j < s.np; j++) {
+					Particle b = s.particles.get(j);
+					double bQx = b.qX;
+					double bQy = b.qY;
+					double bQz = b.qZ;
+					if (i > j) {
+						double tmp = - c * s.g * am * b.mass / Math.pow(s.distance(aQx, aQy, aQz, bQx, bQy, bQz), 3) * s.timeStep;
+						double dPx = (bQx - aQx) * tmp;
+						double dPy = (bQy - aQy) * tmp;
+						double dPz = (bQz - aQz) * tmp;
+						a.pX -= dPx;
+						a.pY -= dPy;
+						a.pZ -= dPz;
+						b.pX += dPx;
+						b.pY += dPy;
+						b.pZ += dPz;
+					}
+				}
+			}
+		}
+	},
 	P {  // Momentum
 		@Override
 		void update (Symplectic s, double c) {
--- src/main/java/uk/me/doitto/orbits/Scenario.java
+++ src/main/java/uk/me/doitto/orbits/Scenario.java
@@ -114,7 +114,7 @@ public enum Scenario {
 			if ((n % scenario.outputInterval) == 0) {
 				System.out.println(scenario.particlesJson());
 				double dbValue = 10.0 * Math.log10(Math.abs(dH / h0));
-				System.err.printf("t:%9.2f, H: %.9e, H0: %.9e, H-: %.9e, H+: %.9e, E: %.1e, ER: %6.1f dBh0%n", n * scenario.timeStep, hNow, h0, hMin, hMax, dH, dbValue);
+				System.err.printf("t:%7.2f, H: %.9e, H0: %.9e, H-: %.9e, H+: %.9e, E: %.1e, ER: %6.1f dBh0%n", n * scenario.timeStep, hNow, h0, hMin, hMax, dH, dbValue);
 				if (dbValue > scenario.errorLimit) {
 					System.err.println("Hamiltonian error, giving up!");
 					return;
